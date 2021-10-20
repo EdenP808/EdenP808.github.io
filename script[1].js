45ab94be76ad3f1c4dafe368113615323e5af09c
EdenP808 <92760976+EdenP808@users.noreply.github.com>
2021-10-20 12:08:21 -1000

    Update script[1].js

--- script[1].js
+++ script[1].js
@@ -1,7 +1,16 @@
 var a = []
-function urName() {
+function addName() {
   var b = document.getElementById('name').value
   a.unshift(b)
   document.getElementById('name').value='';
   window.alert(a)
+}
+
+function urName() {
+  for(var i = 0; i,a.length; i++) {
+    var newP = document.createElement('p');
+    var newText = document.createTextNode(a[i]);
+    newP.appendChild(newText);
+    document.body,appendChild(newP);
+  }
 }
